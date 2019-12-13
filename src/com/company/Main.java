44618ce372d81f81c7cbdbe91a85feed6c8732d4
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-13 20:03:21 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int baseNumber;
+        int exponential;
+        long result;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Base Number");
+        baseNumber = scanner.nextInt();
+        System.out.println("Enter Exponential");
+        exponential = scanner.nextInt();
+        result =  1;
+        for ( int count = 1; count<=exponential; count++ ) {
+            result = result * baseNumber;
+        }
+        System.out.println(result);
+
+    }
+}
